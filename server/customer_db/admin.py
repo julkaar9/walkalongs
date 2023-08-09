4d67d7f2912c7774f9be4d289bbafd0d9e864267
julkaar9 <julkaar9@gmail.com>
2023-08-09 23:35:37 +0530

    sqlonline notebooks

--- server/customer_db/admin.py
+++ server/customer_db/admin.py
@@ -4,8 +4,36 @@ from .hospital_models import Patients, Admissions
 
 # Register your models here.
 
+
+class PatientAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "first_name",
+        "last_name",
+        "gender",
+        "birth_date",
+        "city",
+        "province",
+        "allergies",
+        "height",
+        "weight",
+    )
+
+
+class AdmssionAdmin(admin.ModelAdmin):
+    list_display = (
+        "patient_id",
+        "admission_date",
+        "discharge_date",
+        "diagnosis",
+        "attending_doctor",
+    )
+    ordering = ("patient_id",)
+    list_editable = ("diagnosis",)
+
+
 admin.site.register(Occupation)
 admin.site.register(Person)
 
-admin.site.register(Patients)
-admin.site.register(Admissions)
+admin.site.register(Patients, PatientAdmin)
+admin.site.register(Admissions, AdmssionAdmin)
